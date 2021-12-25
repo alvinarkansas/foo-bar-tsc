b19f02be6aeafbdf2cc3572f8d9e681758d124d4
alvinarkansas <alvinarkansas@gmail.com>
2021-12-25 17:40:16 +0700

    removed unnecessary files

--- Buyer.ts
+++
@@ -1,9 +0,0 @@
-export default class Buyer {
-  name: string;
-  type: string;
-
-  constructor(name: string, type: "VIP" | "regular" | "wholesale") {
-    this.name = name;
-    this.type = type;
-  }
-}
